f72d8029dd240b48ac9ea9a86a40083b4dfced1f
Chandan3678 <rathchandan88@gmail.com>
2025-06-18 14:46:50 +0530

    Create Product.java

---
+++ Day 3/Q5/Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int stockQuantity;
+    private String category;
+    // Getters and Setters
+}
