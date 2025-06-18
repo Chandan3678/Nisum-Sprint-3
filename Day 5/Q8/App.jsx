190d2d803b13412038d1f752379a0379c7950f82
Chandan3678 <rathchandan88@gmail.com>
2025-06-18 16:51:23 +0530

    Create App.jsx

---
+++ Day 5/Q8/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PaginatedProducts from './PaginatedProducts';
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <PaginatedProducts />
+    </div>
+  );
+}
